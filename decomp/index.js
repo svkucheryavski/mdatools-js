07d428dd24ba47b1ff7bde495d723c21218ebc78
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2022-05-04 21:27:25 +0200

    added methods for QR decomposition and inverting a matrix plus corresponding tests

--- decomp/index.js
+++ decomp/index.js
@@ -1,10 +1,95 @@
-import {nrow, ncol} from '../matrix/index.js';
+import {rep, min, subset, seq} from '../stat/index.js';
+import {nrow, ncol, eye, norm2, zeros, vadd, mdot, madd, transpose, msubset, mmult} from '../matrix/index.js';
+import { issquaredmat, islowertrianmat, isuppertrianmat, mreplace, tcrossprod } from '../matrix/index.js';
 
 /**********************************************
  * Functions for decompositions of matrices   *
  **********************************************/
 
+/**
+ * Computes QR decomposition of matrix X using Householder reflections
+ * @param {number[]} X - matrix to decompose (2D Array)
+ * @returns JSON with two matrices, Q and R
+ */
+export function qr(X) {
+
+   const ncols = ncol(X);
+   const nrows = nrow(X);
+   const tMax = min([nrows, ncols]);
+
+   let R = msubset(X, [], []);
+   let Q = eye(nrows);
+
+   for (let ic = 0; ic < tMax; ic++) {
+
+      const x = subset(R[ic], seq(ic + 1, nrows));
+      const n = x.length;
+
+      let e = rep(0, n);
+      e[0] = Math.sign(x[0]) * norm2(x);
+
+      const v = vadd(e, x);
+      const Qt = madd(eye(n), mmult(tcrossprod([v]), -2 / (norm2(v) ** 2)));
+      const Qk = mreplace(eye(nrows), Qt, seq(nrows - n + 1, nrows), seq(nrows - n + 1, nrows));
+
+      Q = mdot(Q, Qk)
+      R = mdot(Qk, R);
+   }
+
+   return {Q, R};
+}
+
+
+/**
+ * Computes Singular Value Decomposition of matrix X
+ * @param {number[]} X — matrix to decompose
+ * @param {number} n — number of components to compute
+ */
 export function svd(X, n) {
    if (!n) n = min([nrow(X) - 1, ncol(X)]);
-
 }
+
+
+/**
+ * Computes inverse of a squared matrix X
+ * @param {number[]} X — squared matrix
+ * @returns inverse of X
+ */
+export function inv(X) {
+
+   if (!issquaredmat(X)) {
+      throw Error("Only squared matrices can be inverted.");
+   }
+
+   if (isuppertrianmat(X)) {
+      return transpose(inv(transpose(X)));
+   }
+
+   if (islowertrianmat(X)) {
+      const n = ncol(X);
+      const I = eye(n);
+      let Y = zeros(n, n);
+
+      for (let k = 1; k <= n; k++) {
+         for (let i = 1; i <= n; i++) {
+            Y[i - 1][k - 1] = (
+               I[i -  1][k - 1] - mdot(
+                     msubset(X, k, seq(1, k - 1)),
+                     msubset(Y, seq(1, k - 1), i)
+                  )
+               ) / X[k - 1][k - 1];
+         }
+      }
+      return Y;
+   }
+
+   // invert matrix using QR transformation
+   const r = qr(X);
+   if (!isuppertrianmat(r.R)) {
+      throw Error("QR decomposition of the matrix returned incorrect result.")
+   }
+
+   return mdot(inv(r.R), transpose(r.Q));
+}
+
+
---
+++ test/tests-decomp.js
@@ -0,0 +1,69 @@
+/******************************************************************
+ *  Tests for vector functions                                    *
+ ******************************************************************/
+
+// import of functions to test
+import {qr, inv} from '../decomp/index.js';
+import {mdot, eye, nrow, tomatrix, transpose, tcrossprod} from '../matrix/index.js';
+import {seq} from '../stat/index.js';
+
+// import dependencies
+import {default as chai} from 'chai';
+import {default as chaiAlmost} from 'chai-almost';
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiAlmost(0.00001));
+describe('Tests for decompositions.', function () {
+
+
+   it('qr() works correctly for squared matrix.', function () {
+      const X = [[12, 6, -4], [-51, 167, 24], [4, -68, -41]];
+      const res = qr(X)
+      it('qr() works correctly.', function () {
+         expect(mdot(res.Q, res.R)).to.be.deep.almost(X)
+         expect(tcrossprod(res.Q, res.Q)).to.be.deep.almost(eye(nrow(X)))
+      });
+   });
+
+
+   it('qr() works correctly for matrix with more columns than rows.', function () {
+      const X = transpose(tomatrix(seq(1, 20), 4, 5));
+      const res = qr(X)
+      it('qr() works correctly.', function () {
+         expect(mdot(res.Q, res.R)).to.be.deep.almost(X)
+         expect(tcrossprod(res.Q, res.Q)).to.be.deep.almost(eye(nrow(X)))
+      });
+   });
+
+
+   it('qr() works correctly for matrix with more rows than columns.', function () {
+      const X = transpose(tomatrix(seq(1, 20), 5, 4));
+      const res = qr(X)
+      it('qr() works correctly.', function () {
+         expect(mdot(res.Q, res.R)).to.be.deep.almost(X)
+         expect(tcrossprod(res.Q, res.Q)).to.be.deep.almost(eye(nrow(X)))
+      });
+   });
+
+
+   it('inv() works correctly.', function () {
+
+      // errors
+      const E = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]];
+      expect(() => inv(E)).to.throw(Error, "Only squared matrices can be inverted.");
+
+      // lower triangular
+      const X1 = [[1, 0, 0, 0], [2, 3, 0, 0], [4, 5, 6, 0], [7, 8, 9, 10]];
+      expect(mdot(X1, inv(X1))).to.be.deep.almost(eye(4))
+
+      // upper triangular
+      const X2 = transpose(X1);
+      expect(mdot(X2, inv(X2))).to.be.deep.almost(eye(4))
+
+      // squared
+      const X3 = [[1, 2, 7, 3], [9, 11, 12, 3], [1, 9, 0, 2], [11, 9, 1, 8]]
+      expect(mdot(X3, inv(X3))).to.be.deep.almost(eye(4))
+
+   })
+});
