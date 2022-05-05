4a42d2bbf9c610ee7b70c7543c7996b5be342a02
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2022-05-05 20:57:35 +0200

    added methods for linear regression model

---
+++ models/index.js
@@ -0,0 +1,128 @@
+import {pf, pt, qt, rep, variance} from '../stat/index.js';
+import {nrow, mmult, ncol, mdot, vdot, vapply, transpose, crossprod, getdiag, vdiv,
+   msubset, isvector, isarray, ismatrix, vsubtract} from '../matrix/index.js';
+import {inv} from '../decomp/index.js';
+
+/**
+ * Fitting a linear model (SLR or MLR)
+ * @param {number[]} X - vector or matrix with predictors
+ * @param {number[]} y - vector with responses
+ * @return JSON with model parameters and performance statistics
+ */
+export function lmfit(X, y) {
+
+   if (!isarray(X)) {
+      throw Error("Argument 'X' must be a matrix or a vector.");
+   }
+
+   if (!isvector(y)) {
+      throw Error("Argument 'y' must be a vector.");
+   }
+
+   const n = nrow(X);
+   if (nrow(y) !== n) {
+      throw Error("Arguments 'X' and 'y' must have the same number of measurements.");
+   }
+
+   if (n <= ncol(X)) {
+      throw Error("Number of measurements must be larger than number of predictors.");
+   }
+
+   if (!ismatrix(X)) {
+      X = [X];
+   }
+
+   // add column of ones for estimation of intercept
+   const Xr = msubset(X, [], []);
+   Xr.unshift(rep(1, n));
+
+   // compute inverse of variance-covariance matrix
+   const R = inv(crossprod(Xr));
+
+   // estimate regression coefficients
+   const estimate = mdot(mdot(R, transpose(Xr)), y)[0];
+
+   // compute predicted y-values and performance statistics
+   const fitted = mdot(Xr, estimate)[0];
+   const stat = regstat(y, fitted, ncol(X));
+
+   // compute standard error and t-values for regression coefficients, H0: beta = 0
+   const coeffse = vapply(getdiag(mmult(R, stat.se**2)), Math.sqrt);
+   const tstat = vdiv(estimate, coeffse);
+
+   // compute critical t-value for confidence intervals
+   const tCrit = qt(0.975, stat.DoF);
+
+   // return JSON with all results
+   return {
+      class: "lm",
+      coeffs: {
+         estimate: estimate,
+         se: coeffse,
+         tstat: tstat,
+         p: tstat.map(t => t > 0 ? 2 * pt(-t, stat.DoF) : 2 * pt(t, stat.DoF)),
+         lower: estimate.map((b, i) => b - tCrit * coeffse[i]),
+         upper: estimate.map((b, i) => b + tCrit * coeffse[i])
+      },
+      fitted: fitted,
+      stat: stat
+   }
+}
+
+/**
+ * Predicts response values based on the fitted model and predictors
+ * @param {JSON} m - regression model (object returned by 'limfit()')
+ * @param {number[]} X - vector or matrix with predictors
+ * @return vector with predicted response values
+ */
+export function lmpredict(m, X) {
+
+   if (!isarray(X)) {
+      throw Error("Argument 'X' must be a matrix or a vector.");
+   }
+
+   if (m.class !== "lm" || !m.coeffs.estimate || m.coeffs.estimate.length < 1) {
+      throw Error("Argument 'm' must be object with MLR model returned by method 'lmfit()'.");
+   }
+
+   if (ncol(X) !== (m.coeffs.estimate.length - 1)) {
+      throw Error("Number of columns in 'X' do not match number of coefficients in model 'm'.");
+   }
+
+
+   if (!ismatrix(X)) {
+      X = [X];
+   }
+
+   const Xr = msubset(X, [], []);
+   Xr.unshift(rep(1, nrow(X)));
+   return mdot(Xr, m.coeffs.estimate)[0];
+}
+
+/**
+ * Computes performance statistics for predicted and reference response values
+ * @param {number[]} y — vector with reference response values
+ * @param {number[]} yp — vector with predicted response values
+ * @return JSON with statistics (adjusted R2, R2, s(e), F-value, p-value)
+ */
+export function regstat(y, yp, p) {
+
+   const n = nrow(y);
+   if (!p) p = 1;
+
+   const e = vsubtract(y, yp);
+   const SSe = vdot(e, e);
+   const SSy = variance(y) * (n - 1);
+   const R2 = (1 - SSe / SSy);
+   const DoF = n - p - 1
+   const F = ((SSy - SSe)/p) / (SSe/DoF);
+
+   return {
+      R2: R2,
+      R2adj: 1 - (1 - R2) * (n - 1) / DoF,
+      Fstat: F,
+      p: 1 - pf(F, p, DoF),
+      DoF: DoF,
+      se: Math.sqrt(SSe/DoF)
+   };
+}
---
+++ test/tests-models.js
@@ -0,0 +1,115 @@
+/******************************************************************
+ *  Tests for modelling methods                                   *
+ ******************************************************************/
+
+// import of functions to test
+import {lmfit, lmpredict, regstat} from '../models/index.js';
+
+// import dependencies
+import {default as chai} from 'chai';
+import {default as chaiAlmost} from 'chai-almost';
+import { lm } from '../stat/index.js';
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiAlmost(0.001));
+
+describe('Tests for linear regression.', function () {
+
+
+   it('lmfit() raises errors correctly.', function () {
+      expect(() => lmfit(1, 2)).to.throw(Error, "Argument 'X' must be a matrix or a vector.");
+      expect(() => lmfit([1], 2)).to.throw(Error, "Argument 'y' must be a vector.");
+      expect(() => lmfit([1], [2])).to.throw(Error, "Number of measurements must be larger than number of predictors.");
+      expect(() => lmfit([1, 2], [2, 3, 4])).to.throw(Error, "Arguments 'X' and 'y' must have the same number of measurements.");
+   });
+
+
+   it('lmfit() works correctly for single predictor.', function () {
+      const X = [1, 2, 3, 4, 5];
+      const y =[10, 19, 31, 39, 55];
+      const m = lmfit(X, y);
+
+      m.class.should.be.equal("lm");
+
+      // check coefficients and related statistics
+      m.coeffs.estimate.length.should.be.equal(2);
+      expect(m.coeffs.estimate).to.be.deep.almost([-2.2, 11.0]);
+      expect(m.coeffs.se).to.be.deep.almost([2.3295, 0.7024]);
+      expect(m.coeffs.tstat).to.be.deep.almost([-0.944, 15.661]);
+      expect(m.coeffs.p).to.be.deep.almost([0.4146, 0.000566]);
+
+      // check predictions
+      m.fitted.length.should.be.equal(y.length);
+      expect(m.fitted).to.be.deep.almost([8.8, 19.8, 30.8, 41.8, 52.8]);
+
+      // check performance statistics
+      m.stat.DoF.should.be.equal(3);
+      expect(m.stat.se).to.be.almost(2.221);
+      expect(m.stat.R2).to.be.almost(0.9879);
+      expect(m.stat.R2adj).to.be.almost(0.9839);
+      expect(m.stat.Fstat).to.be.almost(245.270);
+      expect(m.stat.p).to.be.almost(0.0005658);
+
+   });
+
+
+   it('lmfit() works correctly for multiple predictors.', function () {
+      const X = [[1, 2, 3, 4, 5], [10, 20, 10, 20, 10]];
+      const y =[10, 19, 31, 39, 55];
+      const m = lmfit(X, y);
+
+      m.class.should.be.equal("lm");
+
+      // check coefficients and related statistics
+      m.coeffs.estimate.length.should.be.equal(3);
+      expect(m.coeffs.estimate).to.be.deep.almost([2.0, 11.0, -0.3]);
+      expect(m.coeffs.se).to.be.deep.almost([2.3381, 0.4472, 0.1291]);
+      expect(m.coeffs.tstat).to.be.deep.almost([0.855, 24.597, -2.324]);
+      expect(m.coeffs.p).to.be.deep.almost([0.48245, 0.00165, 0.14576]);
+
+      // check predictions
+      m.fitted.length.should.be.equal(y.length);
+      expect(m.fitted).to.be.deep.almost([10, 18, 32, 40, 54]);
+
+      // check performance statistics
+      m.stat.DoF.should.be.equal(2);
+      expect(m.stat.se).to.be.almost(1.414);
+      expect(m.stat.R2).to.be.almost(0.9967);
+      expect(m.stat.R2adj).to.be.almost(0.9935);
+      expect(m.stat.Fstat).to.be.almost(305.2);
+      expect(m.stat.p).to.be.almost(0.003266);
+
+   });
+
+
+   it('lmpredict() raises errors correctly.', function () {
+
+      const X = [[1, 2, 3, 4, 5], [10, 20, 10, 20, 10]];
+      const y = [10, 19, 31, 39, 55];
+      const m = lmfit(X, y);
+
+      expect(() => lmpredict({}, 2)).to.throw(Error, "Argument 'X' must be a matrix or a vector.");
+      expect(() => lmpredict({}, [2, 3])).to.throw(Error, "Argument 'm' must be object with MLR model returned by method 'lmfit()'.");
+      expect(() => lmpredict(m, [2, 3])).to.throw(Error, "Number of columns in 'X' do not match number of coefficients in model 'm'");
+   })
+
+
+   it('lmpredict() works correctly.', function () {
+
+      const X1 = [1, 2, 3, 4, 5];
+      const y1 =[10, 19, 31, 39, 55];
+      const m1 = lmfit(X1, y1);
+
+      const yp1 = lmpredict(m1, X1)
+      yp1.should.be.eql(m1.fitted);
+
+      const X2 = [[1, 2, 3, 4, 5], [10, 20, 10, 20, 10]];
+      const y2 =[10, 19, 31, 39, 55];
+      const m2 = lmfit(X2, y2);
+
+      const yp2 = lmpredict(m2, X2)
+      yp2.should.be.eql(m2.fitted);
+   });
+
+});
